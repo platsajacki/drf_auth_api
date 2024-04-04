2f15ec15f37d8613cb9b93d1d72ff2fbbf0bf051
Вячеслав Менюхов <menyukhov@bk.ru>
2024-04-04 09:00:25 +0300

    Update users view tests.

--- tests/test_user_viewset.py
+++ tests/test_user_viewset.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from api.serializers import UserSerializer
 from api.views import UserViewSet
 from users.models import User
 
@@ -21,4 +22,49 @@ def test_unauthorized_users_dont_have_access_to_list(as_anon: APIClient):
 def test_unauthorized_users_dont_have_access_to_object(as_anon: APIClient, user: User):
     for method in [as_anon.get, as_anon.patch, as_anon.delete]:
         response = method(reverse('api:users:user-detail', kwargs={'pk': user.id}))
+
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.usefixtures('users')
+def test_authorized_user_list(as_user: APIClient):
+    response = as_user.get(reverse('api:users:user-list'))
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == UserSerializer(User.objects.all(), many=True).data
+
+
+def test_authorized_user_detail(as_user: APIClient, user: User):
+    response = as_user.get(reverse('api:users:user-detail', kwargs={'pk': user.id}))
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == UserSerializer(user).data
+
+
+def test_another_user_cannot_update_object(as_user: APIClient, users: list[User]):
+    response = as_user.patch(reverse('api:users:user-detail', kwargs={'pk': users[0].id}))
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_another_user_cannot_delete_object(as_user: APIClient, users: list[User]):
+    response = as_user.delete(reverse('api:users:user-detail', kwargs={'pk': users[0].id}))
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_user_can_update_object(as_user: APIClient, user: User):
+    response = as_user.patch(
+        reverse('api:users:user-detail', kwargs={'pk': user.id}),
+        data={'first_name': 'first_name'},
+    )
+    user.refresh_from_db()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == UserSerializer(user).data
+
+
+def test_user_can_delete_object(as_user: APIClient, user: User):
+    response = as_user.delete(reverse('api:users:user-detail', kwargs={'pk': user.id}))
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
